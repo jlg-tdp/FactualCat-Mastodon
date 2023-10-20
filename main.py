e774f809fc3875f0d715abd4408de14cb00177a1
jlg-tdp <148490139+jlg-tdp@users.noreply.github.com>
2023-10-20 12:56:26 -0400

    Update main.py
    Adding image support

--- main.py
+++ main.py
@@ -9,14 +9,26 @@ mastodon = Mastodon(
     api_base_url="https://botsin.space"
 )
 
+
 def random_fact():
     fact = requests.get("https://catfact.ninja/fact?max_length=280").json()
     return fact["fact"]
 
 
+def get_cat_pic():
+    url = "https://api.thecatapi.com/v1/images/search"
+    cats = requests.request("GET", url).json()
+    cat_pic = cats[0]["url"]
+    img_data = requests.get(cat_pic).content
+    with open("cat_pic.jpg", "wb") as handler:
+        handler.write(img_data)
+    return mastodon.media_post("cat_pic.jpg")
+
+
 def main():
     fact = random_fact()
-    mastodon.status_post(status=fact)
+    media = get_cat_pic()
+    mastodon.status_post(status=fact, media_ids=media)
 
 
 if __name__ == "__main__":
