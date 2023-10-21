f4147e7324122b85fc1821646fd7a40f05c439ab
jlg-tdp <148490139+jlg-tdp@users.noreply.github.com>
2023-10-21 18:14:02 -0400

    Changing print to return

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ mastodon = Mastodon(
 
 def random_fact():
     fact = requests.get("https://meowfacts.herokuapp.com/").json()
-    print(fact['data'][0])
+    return(fact['data'][0])
 
 
 def get_cat_pic():
@@ -33,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
